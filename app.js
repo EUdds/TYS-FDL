aa53dbdb859ddef8d3cf1181cc2bd300392e846a
Eric Udlis <udlis.eric@gmail.com>
2020-03-25 23:38:57 -0500

    Holy Shit Inital commit

---
+++ app.js
@@ -0,0 +1,106 @@
+require('dotenv').config();
+
+const PORT = process.env.PORT || 7000;
+const EXPRESS = require('express');
+const EXPHBS = require('express-handlebars');
+const BODY_PARSER = require('body-parser');
+const HTTP = require('http');
+const DB = require('diskdb');
+const MULTER = require('multer');
+const passwordProtect = require('express-password-protect');
+const fs = require('fs');
+DB.connect('./db', ['resturants', 'people']);
+
+const upload = MULTER({dest: './assets/images'});
+
+const APP = module.exports.app = EXPRESS();
+const SERVER = HTTP.createServer(APP);
+
+SERVER.listen(PORT, () => {
+  console.log(`The pary is happening on ${PORT}, who do you know here?`);
+});
+
+APP.set('view engine', 'handlebars');
+APP.engine('handlebars', EXPHBS({defaultLayout: 'main'}));
+
+APP.use(EXPRESS.static('assets'));
+APP.use(BODY_PARSER.urlencoded({extended: true}));
+
+APP.get('*', (req, res, next) => {
+  res.locals.user = req.user || null;
+  next();
+});
+
+APP.get('/', (req, res) => {
+   let resList = DB.resturants.find();
+   console.log(resList);
+  res.render('welcome', {
+    resturant: resList
+  });
+});
+
+APP.get('/add', (req, res) => {
+    res.render('add');
+});
+
+APP.get('/tip', (req, res) => {
+    let resturant = req.query.q;
+    let place = DB.resturants.findOne({shortName: resturant});
+    console.log(place);
+    let people = DB.people.find({place: resturant});
+    res.render('tip', {
+        place: place,
+        people: people
+    });
+})
+
+// Password Protected Routes
+const config = {
+    username: process.env.username,
+    password: process.env.password,
+    maxAge: 600000 // 10 Minutes
+}
+APP.use(passwordProtect(config));
+
+APP.post('/', (req, res) => {
+    res.redirect('/insert');
+});
+
+
+APP.get('/insert', (req, res) => {
+    res.render('adminAdd');
+});
+
+
+APP.post('/insert/resturant', upload.single('img'), (req, res) => {
+    let longName = req.body.longName;
+    let shortName = req.body.shortName;
+    let img = req.file;
+    fs.rename('assets/images/' + req.file.filename, 'assets/images/' + shortName +'.png', (err) => {
+        if (err) console.error(err);
+    });
+    let resturant = {
+        longName: longName,
+        shortName: shortName
+    };
+    DB.resturants.save(resturant);
+
+    res.redirect('/');
+});
+
+APP.post('/insert/person',upload.none(), (req, res) => {
+    let venmo = req.body.venmo;
+    let paypal = req.body.paypal;
+    let cashapp = req.body.cashapp; 
+    let name = req.body.Name;
+    let place = req.body.place;
+    let person = {
+        name: name,
+        place: place,
+        venmo: venmo,
+        paypal: paypal,
+        cashapp: cashapp
+    };
+    DB.people.save(person);
+    res.redirect('/');
+});
